ca0aecef2f2c83d48061aa665556bfcbea3e6ee4
regfe89 <marat.ismailov889@gmail.com>
2022-10-24 20:23:36 +0600

    Update Cargo.toml, contemp_fiction_top100.txt, and 67 more files...

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-use std::cell::RefCell;
-use std::error;
-use std::rc::Rc;
-
+// use std::cell::RefCell;
+// use std::error;
+// use std::rc::Rc;
+use std::io::prelude::*;
 use wasm_bindgen::{prelude::*, JsCast};
-use web_sys::{console, Event};
-use web_sys::{Document, Element};
+use web_sys::{Event, Element, Request, RequestInit, RequestMode, Response};
+use wasm_bindgen_futures::JsFuture;
+// use web_sys::{Document, Element};
 
 macro_rules! console_log {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
@@ -24,7 +25,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 // Called by our JS entry point to run the example
 #[wasm_bindgen(start)]
-pub fn run() -> Result<(), JsValue> {
+pub async fn main() -> Result<(), JsValue> {
     // proper errors in console
     // console_error_panic_hook::set_once();
 
@@ -33,6 +34,9 @@ pub fn run() -> Result<(), JsValue> {
 
     // Use `web_sys`'s global `window` function to get a handle on the global
     // window object.
+    // let mut contents = "ttttt";
+    // println!("contents are {}", contents);
+    
     let window = web_sys::window().expect("no global `window` exists");
     let document = window.document().expect("should have a document on window");
     let body = document.body().expect("document should have a body");
@@ -41,44 +45,104 @@ pub fn run() -> Result<(), JsValue> {
     let input: Element = document.create_element("input")?;
     input.set_attribute("placeholder", "guess a word")?;
 
-    let container = Rc::new(RefCell::new(document.create_element("div").unwrap()));
+    let text_block = document.create_element("p")?;
+    text_block.set_id("text-block");
+
+    let entered_value = document.create_element("p")?;
+    entered_value.set_id("entered-value");
+
+    body.append_child(&input);
+    body.append_child(&text_block);
+    body.append_child(&entered_value);
+
+
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
+
+    let url = "dictionary/rlnc_top100.txt";
+
+    let request = Request::new_with_str_and_init(&url, &opts)?;
+
+    // request
+    //     .headers()
+    //     .set("Accept", "application/vnd.github.v3+json")?;
+
+    let resp_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    let jstext = JsFuture::from(resp.text()?).await?;
+    let text = &jstext.as_string().unwrap();
+    let copy = text.clone();
+    // let text2 = text.as_str();
+
+    // console_log!("{}", text);
+
+
+
+
+    // let container = Rc::new(RefCell::new(document.create_element("div").unwrap()));
     // let dom_board = our_board.borrow().to_dom(&document)?;
     // container.borrow_mut().append_child(&dom_board)?;
 
     // let board_ref = Rc::clone(&board);
-    let container_ref = Rc::clone(&container);
+    // let container_ref = Rc::clone(&container);
     let cb = Closure::wrap(Box::new(move |e: Event| {
         let input = e
             .current_target()
             .unwrap()
             .dyn_into::<web_sys::HtmlInputElement>()
             .unwrap();
-
-        // if input.value().len() == 5 {
-        //     &board_ref.borrow_mut().guess(&input.value());
-        //     let document = window.document().expect("should have a document on window");
-        //     // let dom_board2 = board.borrow().to_dom(&document).unwrap();
-        //     container_ref.borrow_mut().set_text_content("".into());
-        //     container_ref.borrow_mut().append_child(&dom_board2).unwrap();
-        // }
-        console_log!("{:?}", input.value());
         let text_value = &input.value();
-        // text_value = &input.value();
-        console_log!("this is {:?}", &text_value);
+        // cons(&text_value);
+        change_text_block(&copy, &text_value);
     }) as Box<dyn FnMut(_)>);
     input.add_event_listener_with_callback("input", &cb.as_ref().unchecked_ref())?;
     cb.forget();
 
-    body.append_child(&container.borrow())?;
-    body.append_child(&input)?;
+    // body.append_child(&contai
 
-
+    document
+    .get_element_by_id("text-block")
+    .expect("#text-block should exist")
+    .dyn_into::<web_sys::HtmlElement>()
+    .expect("#text-block should be a HtmlElement")
+    .set_inner_html(&text);
 
     Ok(())
 }
 
 
-#[wasm_bindgen]
-pub fn add(a: u32, b: u32) -> u32 {
-    a + b
+// #[wasm_bindgen]
+// pub fn add(a: u32, b: u32) -> u32 {
+//     a + b
+// }
+
+// fn cons(text: &str) {
+//     console_log!("this is text: {}", &text);
+// }
+
+fn change_text_block(value: &str, text: &str) {
+    console_log!("test");
+    let window = web_sys::window().expect("no global `window` exists");
+    // let document = web_sys::window().unwrap().document().unwrap();
+    let document = window.document().expect("should have a document on window");
+    document
+    .get_element_by_id("text-block")
+    .expect("#text-block should exist")
+    .dyn_into::<web_sys::HtmlElement>()
+    .expect("#text-block should be a HtmlElement")
+    .set_inner_html(&value);
+
+    document
+    .get_element_by_id("entered-value")
+    .expect("#text-block should exist")
+    .dyn_into::<web_sys::HtmlElement>()
+    .expect("#text-block should be a HtmlElement")
+    .set_inner_html(&text);
+    // body.append_child(&text_block)?;
+    // Ok(())
 }
